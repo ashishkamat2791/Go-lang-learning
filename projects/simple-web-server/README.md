119be81d7647447cb919d1e162d2c0b36caff2d5
ashishkamat2791 <ashishkamat91@gmail.com>
2022-06-28 18:16:00 +0530

    added project

---
+++ projects/simple-web-server/README.md
@@ -0,0 +1,3 @@
+# Simple web server
+
+![architecture](Architecture.jpg)
--- projects/simple-web-server/main.go
+++ projects/simple-web-server/main.go
@@ -1,6 +1,40 @@
-
 package main
 
-func main(){
-	fmt.println("hello world!")
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusFound)
+	}
+	fmt.Fprintf(w, "Hello world!")
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request succesful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address=%s\n", address)
+}
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
